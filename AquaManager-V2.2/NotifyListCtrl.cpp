a38f921ce98a47ae902d0efbd6f869c6d879b4c9
Xiaodong Han <xiaodong.h.han@gmail.com>
2017-10-18 15:34:21 +0800

    add single NotifyListCtrl class.

---
+++ AquaManager-V2.2/NotifyListCtrl.cpp
@@ -0,0 +1,30 @@
+// NotifyListCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "AquaManager-V2.2.h"
+#include "NotifyListCtrl.h"
+
+
+// CNotifyListCtrl
+
+IMPLEMENT_DYNAMIC(CNotifyListCtrl, CListCtrl)
+
+CNotifyListCtrl::CNotifyListCtrl()
+{
+
+}
+
+CNotifyListCtrl::~CNotifyListCtrl()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CNotifyListCtrl, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// CNotifyListCtrl message handlers
+
+
---
+++ AquaManager-V2.2/NotifyListCtrl.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// CNotifyListCtrl
+
+class CNotifyListCtrl : public CListCtrl
+{
+	DECLARE_DYNAMIC(CNotifyListCtrl)
+
+public:
+	CNotifyListCtrl();
+	virtual ~CNotifyListCtrl();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
