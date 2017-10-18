045a3bc8c7b70e7adcca7519a002819a2dabcc40
Xiaodong Han <xiaodong.h.han@gmail.com>
2017-10-18 15:35:18 +0800

    add single DeviceListCtrl class.

---
+++ AquaManager-V2.2/DeviceListCtrl.cpp
@@ -0,0 +1,30 @@
+// DeviceListCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "AquaManager-V2.2.h"
+#include "DeviceListCtrl.h"
+
+
+// CDeviceListCtrl
+
+IMPLEMENT_DYNAMIC(CDeviceListCtrl, CListCtrl)
+
+CDeviceListCtrl::CDeviceListCtrl()
+{
+
+}
+
+CDeviceListCtrl::~CDeviceListCtrl()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CDeviceListCtrl, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// CDeviceListCtrl message handlers
+
+
---
+++ AquaManager-V2.2/DeviceListCtrl.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// CDeviceListCtrl
+
+class CDeviceListCtrl : public CListCtrl
+{
+	DECLARE_DYNAMIC(CDeviceListCtrl)
+
+public:
+	CDeviceListCtrl();
+	virtual ~CDeviceListCtrl();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
