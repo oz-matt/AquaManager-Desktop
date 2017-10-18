16f950635d16327f407a3ad4881ba54ab64f36fe
Xiaodong Han <xiaodong.h.han@gmail.com>
2017-10-18 15:34:40 +0800

    add single GeofenceListCtrl class.

---
+++ AquaManager-V2.2/GeofenceListCtrl.cpp
@@ -0,0 +1,30 @@
+// GeofenceListCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "AquaManager-V2.2.h"
+#include "GeofenceListCtrl.h"
+
+
+// CGeofenceListCtrl
+
+IMPLEMENT_DYNAMIC(CGeofenceListCtrl, CListCtrl)
+
+CGeofenceListCtrl::CGeofenceListCtrl()
+{
+
+}
+
+CGeofenceListCtrl::~CGeofenceListCtrl()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CGeofenceListCtrl, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// CGeofenceListCtrl message handlers
+
+
---
+++ AquaManager-V2.2/GeofenceListCtrl.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// CGeofenceListCtrl
+
+class CGeofenceListCtrl : public CListCtrl
+{
+	DECLARE_DYNAMIC(CGeofenceListCtrl)
+
+public:
+	CGeofenceListCtrl();
+	virtual ~CGeofenceListCtrl();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
